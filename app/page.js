72079db6db88abff6c1a1087d5d04ca04fcb09fe
Reda El Marzouki <relmarzouki@gmail.com>
2024-02-28 13:58:37 +0300

    add: hero page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-
+import Hero from "./components/Hero";
 
 export default function Home() {
   return (
     <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
-      <h1>Home</h1>
+      <Hero />
     </div>
   );
 }
